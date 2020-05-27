ae97e0c73f751c36f7681a920934fea7770a4f3d
BoozeBox <61587470+BoozeBox@users.noreply.github.com>
2020-05-27 18:08:18 +0200

    Added the Public file
    This is the main BoozeBox website file

---
+++ public/js/main.js
@@ -0,0 +1,22 @@
+
+// Entrance animations 
+
+
+
+// Button animations
+
+// let productBtn = document.querySelector("#shop .product-btn");
+// let productTxt = document.querySelector(".product-btn span");
+// let productArw = document.querySelector(".arrow");
+
+// productBtn.addEventListener("mouseenter", function() {
+//     gsap.to(productBtn, { top: "-10%", ease: "back.inOut" });
+//     gsap.to(productTxt, { scale: 1.2, ease: "back.inOut" });
+//     gsap.to(productArw, { scale: 1.2, ease: "back.inOut" });
+// })
+
+// productBtn.addEventListener("mouseleave", function() {
+//     gsap.to(productBtn, { top: "-12.5%", ease: "back.inOut" });
+//     gsap.to(productTxt, { scale: 1, ease: "back.inOut" });
+//     gsap.to(productArw, { scale: 1, ease: "back.inOut" });
+// })
